026e0d987c28ea87f4fc76d8995dcf721c9c5cf8
Jia-Jia Zhu <135554427+zhus-on-first@users.noreply.github.com>
2023-10-02 18:52:43 -0700

    lab done

--- server/app.py
+++ server/app.py
@@ -21,6 +21,30 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route("/earthquakes/<int:id>")
+def earthquake_by_id(id):
+    earthquake = Earthquake.query.filter(Earthquake.id == id).first()
+
+    if earthquake is not None:
+        body = earthquake.to_dict()
+        status = 200
+    else:
+        body = {"message": f"Earthquake {id} not found."}
+        status = 404
+    
+    return make_response(body, status)
+
+@app.route("/earthquakes/magnitude/<float:magnitude>")
+def earthquake_by_magnitude(magnitude):
+    earthquakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+
+    earthquakes_list = [earthquake.to_dict() for earthquake in earthquakes]
+
+    body = {"count": len(earthquakes_list),
+            "quakes": earthquakes_list
+            }
+    
+    return make_response(body, 200)
 
 
 if __name__ == '__main__':
--- server/models.py
+++ server/models.py
@@ -7,3 +7,13 @@ metadata = MetaData()
 db = SQLAlchemy(metadata=metadata)
 
 # Add models here
+class Earthquake(db.Model, SerializerMixin):
+    __tablename__ = "earthquakes"
+
+    id = db.Column(db.Integer, primary_key=True)
+    magnitude = db.Column(db.Float)
+    location = db.Column(db.String)
+    year = db.Column(db.Integer)
+
+    def __repr__(self):
+        return f"<Earthquake {self.id}, {self.magnitude}, {self.location}, {self.year}>"
